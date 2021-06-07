7c517d4e4844a1fd6ceee34ae0fbca2d2ef3274f
Türkay Tunç <turkaytunc00@gmail.com>
2021-06-07 20:37:20 +0300

    add: controller

--- routes/routes.go
+++ routes/routes.go
@@ -1,9 +1,10 @@
 package routes
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/turkaytunc/go-web-fiber/controllers"
+)
 
 func Setup(app *fiber.App) {
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World 👋!")
-	})
+	app.Get("/", controllers.Hello)
 }
