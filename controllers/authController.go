6ea8123510be8a0af7edefb8183fe22bbd0edfd6
Türkay Tunç <turkaytunc00@gmail.com>
2021-06-22 10:21:53 +0300

    add: login route

--- controllers/authController.go
+++ controllers/authController.go
@@ -18,8 +18,33 @@ func Register(c *fiber.Ctx) error {
 
 	hashedPassword, _ := bcrypt.GenerateFromPassword([]byte(data["password"]), 14)
 
-	user := models.User{FirstName: data["firstName"], LastName: data["lastName"], Email: data["email"], Password: hashedPassword}
+	user := models.User{FirstName: data["firstName"], LastName: data["lastName"], Email: data["email"], Password: string(hashedPassword)}
 
 	database.Connect().Create(&user)
 	return c.Status(200).JSON(&user)
+}
+
+func Login(c *fiber.Ctx) error {
+	var data map[string]string
+
+	err := c.BodyParser(&data)
+	if err != nil {
+		return err
+	}
+
+	var user models.User
+	database.Connect().Where("email = ?", data["email"]).First(&user)
+
+	if user.Id == 0 {
+		c.Status(404)
+		return c.JSON(fiber.Map{"message": "user not found"})
+	}
+
+	passErr := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(data["password"]))
+
+	if passErr != nil {
+		return c.Status(400).JSON(fiber.Map{"message": "password is incorrect"})
+	}
+
+	return c.JSON(&user)
 }
--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ go 1.16
 
 require (
 	github.com/gofiber/fiber/v2 v2.12.0
-	golang.org/x/crypto v0.0.0-20210616213533-5ff15b29337e // indirect
+	golang.org/x/crypto v0.0.0-20210616213533-5ff15b29337e
 	gopkg.in/yaml.v2 v2.3.0 // indirect
 	gorm.io/driver/postgres v1.1.0
 	gorm.io/gorm v1.21.10
