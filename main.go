a73554233e1577863000fa36ecef4534916a981c
Türkay Tunç <turkaytunc00@gmail.com>
2021-06-23 20:19:27 +0300

    add: cors

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/turkaytunc/go-web-fiber/database"
 	"github.com/turkaytunc/go-web-fiber/routes"
 )
@@ -11,6 +12,9 @@ func main() {
 	database.Connect()
 
 	app := fiber.New()
+
+	app.Use(cors.New(cors.Config{AllowCredentials: true}))
+
 	routes.Setup(app)
 
 	app.Listen(":4000")
